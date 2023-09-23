55284953a0f8e38e4ae09408c521cbe632f356f4
김준서(풀스택 1회차) <jykim9868@gmail.com>
2023-09-23 05:28:59 +0000

    Feat : 모달 래퍼 UI 완료

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,17 +1,71 @@
+import { useState } from 'react';
 import cn from 'classnames';
 
-import { Button, Typography } from '@goorm-dev/gds-challenge';
+import {
+	Button,
+	Typography,
+	Modal,
+	CarouselIndicators,
+} from '@goorm-dev/gds-challenge';
 
 import styles from './Header.module.scss';
 
 const Header = () => {
+	const [isOpen, setIsOpen] = useState(false);
+	const [activeIndex, setActiveIndex] = useState(0);
+	const toggle = () => {
+		setIsOpen((prev) => {
+			return !prev;
+		});
+	};
+
+	const handleNext = () => {
+		setActiveIndex((prev) => {
+			return prev + 1;
+		});
+	};
+
+	const handlePrev = () => {
+		setActiveIndex((prev) => {
+			return prev - 1;
+		});
+	};
+
 	return (
 		<header className={cn(styles.header)}>
 			<div className={cn(styles.contents)}>
 				<Typography token="h5">
 					구름톤 챌린지 참여자 정보 수집
 				</Typography>
-				<Button size="lg">설문조사 참여하기</Button>
+				<Button size="lg" onClick={toggle}>
+					설문조사 참여하기
+				</Button>
+				<Modal isOpen={isOpen} centered>
+					<Modal.Header toggle={toggle} />
+					<Modal.Body>
+						{activeIndex === 0 && <div>1번 문항</div>}
+						{activeIndex === 1 && <div>2번 문항</div>}
+						{activeIndex === 2 && <div>3번 문항</div>}
+						{activeIndex === 3 && <div>4번 문항</div>}
+					</Modal.Body>
+					<Modal.Footer between>
+						<CarouselIndicators
+							activeIndex={activeIndex}
+							length={4}
+						/>
+						<div className={cn(styles.buttons)}>
+							{activeIndex > 0 && (
+								<Button onClick={handlePrev} color="link">
+									이전
+								</Button>
+							)}
+							{activeIndex < 3 && (
+								<Button onClick={handleNext}>다음</Button>
+							)}
+							{activeIndex === 3 && <Button>제출하기</Button>}
+						</div>
+					</Modal.Footer>
+				</Modal>
 			</div>
 		</header>
 	);
